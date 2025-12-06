6bb74c16773c32e317cb440deb7aa9ec9e53117a
ghfbsd <ghfbsd@gmail.com>
2025-12-06 19:47:19 +0000

    Fix non-printing character in routing display

--- BTrain/Layouts/Parser/LayoutASCIIProducer.swift
+++ BTrain/Layouts/Parser/LayoutASCIIProducer.swift
@@ -231,9 +231,9 @@ final class LayoutASCIIProducer {
                 return stringFrom(train)
             case .wagon:
                 if let tail = train.positions.tail, tail.index == position, tail.blockId == block.id {
-                    return "􀼰\(train.id)"
+                    return "►\(train.id)"
                 } else {
-                    return "􀼯\(train.id)"
+                    return "►\(train.id)"
                 }
             }
         } else {
@@ -260,7 +260,7 @@ final class LayoutASCIIProducer {
         if !train.directionForward {
             text += "!"
         }
-        text += "􀼮\(train.id)"
+        text += "►\(train.id)"
         return text
     }
 
